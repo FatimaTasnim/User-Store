0b7684672eaac4bb38040a2618ce95c1eee8d023
FatimaTasnim <tasnim.roujat@gmail.com>
2020-08-09 01:37:38 +0600

    Create basic flask server
    Closes #1

--- app.py
+++ app.py
@@ -1,14 +1,7 @@
 from flask import Flask
 
+
 app = Flask(__name__)
 
 
-@app.route('/')
-
-
-def index():
-    return 'User Store Application'
-
-
-if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
+app.run()
