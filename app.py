5092bf4bd20c3451038c7924c8d93c1f123a6fa3
FatimaTasnim <tasnim.roujat@gmail.com>
2020-08-09 01:45:11 +0600

    Configure mongodb
    Closes #2

--- app.py
+++ app.py
@@ -1,7 +1,15 @@
 from flask import Flask
+from database.db import initialize_db
 
 
 app = Flask(__name__)
+
+app.config['MONGODB_SETTINGS'] = {
+    'host': 'mongodb://localhost/userStore'
+}
+
+
+initialize_db(app)
 
 
 app.run()
