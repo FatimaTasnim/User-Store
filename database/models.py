538e33728cb451e6e9a54bc5373f7f18372ac136
FatimaTasnim <tasnim.roujat@gmail.com>
2020-08-09 20:42:00 +0600

    Add UUID Generator

--- database/models.py
+++ database/models.py
@@ -1,8 +1,11 @@
 from .db import db
+import uuid
 
 
+def generate_uuid():
+    return (uuid.uuid4().hex)
 class User(db.Document):
-    id = db.StringField(required = True)
+    user_id = db.StringField(unique=True, default=generate_uuid, immutable=True)
     first_name = db.StringField(required=True)
     last_name = db.StringField(required=True)
     role = db.ListField(db.StringField(), required=True)
