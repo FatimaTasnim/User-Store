6c07f00b86dd78d6d48e85bfcd6ab3dedaf34f58
FatimaTasnim <tasnim.roujat@gmail.com>
2020-08-09 20:43:14 +0600

    Add immutable property in User model

--- database/models.py
+++ database/models.py
@@ -4,13 +4,19 @@ import uuid
 
 def generate_uuid():
     return (uuid.uuid4().hex)
+
+
+USER_ROLE = ("PARENT", "CHILD")
+
+
 class User(db.Document):
     user_id = db.StringField(unique=True, default=generate_uuid, immutable=True)
     first_name = db.StringField(required=True)
-    last_name = db.StringField(required=True)
-    role = db.ListField(db.StringField(), required=True)
-    parent = db.StringField(required=False) 
+    last_name = db.StringField(required=True) 
+    role = db.StringField(choices=USER_ROLE, required=True, immutable=True)
+    parent = db.StringField(required=False, immutable=True)
     state = db.StringField(required=False)
     street = db.StringField(required=False)
     city = db.StringField(required=False)
     zip = db.StringField(required=False)
+
