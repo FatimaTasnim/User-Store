26441990e1664455349784606ad8e884e8efadab
FatimaTasnim <tasnim.roujat@gmail.com>
2020-08-09 21:09:22 +0600

    Python pep8 style format

--- app.py
+++ app.py
@@ -1,91 +1,88 @@
-from flask import Flask, request, Response
+from flask import Flask, Response, request
+
 from database.db import initialize_db
 from database.models import User
-
 
 app = Flask(__name__)
 
 
-app.config['MONGODB_SETTINGS'] = {
-    'host': 'mongodb://localhost/userStore'
-}
+app.config["MONGODB_SETTINGS"] = {"host": "mongodb://localhost/userStore"}
 
 
 initialize_db(app)
 
 
-@app.route('/api/users', methods=['POST'])
+@app.route("/api/users", methods=["POST"])
 def add_user():
     body = request.get_json()
     user = User()
-    user['first_name'] = body['first_name']
-    user['last_name'] = body['last_name']
-    user['role'] = body['role']
+    user["first_name"] = body["first_name"]
+    user["last_name"] = body["last_name"]
+    user["role"] = body["role"]
 
-    if body['role'] == 'PARENT':
-        if 'street' in body:
-            user['street'] = body['street']
-        if 'state' in body:
-            user['state'] = body['state']
-        if 'city' in body:
-            user['city'] = body['city']
-        if 'zip' in body:
-            user['zip'] = body['zip']
+    if body["role"] == "PARENT":
+        if "street" in body:
+            user["street"] = body["street"]
+        if "state" in body:
+            user["state"] = body["state"]
+        if "city" in body:
+            user["city"] = body["city"]
+        if "zip" in body:
+            user["zip"] = body["zip"]
         user.save()
         return "Your data is stored properly", 200
-    elif body['role'] == 'CHILD':
+    elif body["role"] == "CHILD":
         try:
-            body['parent']
-            if body['parent'] == "":
-                return {'error': "please enter parent id"}, 400
+            body["parent"]
+            if body["parent"] == "":
+                return {"error": "please enter parent id"}, 400
             try:
-                User.objects.get(user_id=body['parent'])
-                user['parent'] = body['parent']
+                User.objects.get(user_id=body["parent"])
+                user["parent"] = body["parent"]
                 user.save()
                 return "Your data stored properly", 201
             except:
-                return {'error': "please enter a registered parent id"}, 400
+                return {"error": "please enter a registered parent id"}, 400
         except:
-            return {'error': "please enter parent id"}, 400
+            return {"error": "please enter parent id"}, 400
 
 
-@app.route('/api/users/<id>', methods=['DELETE'])
+@app.route("/api/users/<id>", methods=["DELETE"])
 def delete_user(id):
     try:
         User.objects.get(user_id=id).delete()
         User.objects(parent=id).delete()
-        return 'User and associate childs are deleted', 200
+        return "User and associate childs are deleted", 200
     except:
-        return {'error': "Could not delete user"}, 400
+        return {"error": "Could not delete user"}, 400
 
 
-@app.route('/api/users/<id>', methods=['PATCH'])
+@app.route("/api/users/<id>", methods=["PATCH"])
 def update_user(id):
     try:
         body = request.get_json()
         User.objects.get(user_id=id).update(**body)
-        return 'Data is updated', 200
+        return "Data is updated", 200
     except:
-        return {'error': 'User ID, role & parent cannot be changed'}, 400
+        return {"error": "User ID, role & parent cannot be changed"}, 400
 
 
-@app.route('/api/users/<id>', methods=['GET'])
+@app.route("/api/users/<id>", methods=["GET"])
 def get_user(id):
     try:
         users = User.objects.get(user_id=id).to_json()
         return Response(users, mimetype="application/json", status=200)
     except:
-        return {'error': "No user found"}, 400
+        return {"error": "No user found"}, 400
 
 
-@app.route('/api/users', methods=['GET'])
+@app.route("/api/users", methods=["GET"])
 def get_all_users():
     try:
         users = User.objects().to_json()
         return Response(users, mimetype="application/json", status=200)
     except:
-        return {'error': "No user found"}, 400
+        return {"error": "No user found"}, 400
 
 
 app.run()
-
--- database/models.py
+++ database/models.py
@@ -1,9 +1,10 @@
-from .db import db
 import uuid
+
+from .db import db
 
 
 def generate_uuid():
-    return (uuid.uuid4().hex)
+    return uuid.uuid4().hex
 
 
 USER_ROLE = ("PARENT", "CHILD")
@@ -12,11 +13,10 @@ USER_ROLE = ("PARENT", "CHILD")
 class User(db.Document):
     user_id = db.StringField(unique=True, default=generate_uuid, immutable=True)
     first_name = db.StringField(required=True)
-    last_name = db.StringField(required=True) 
+    last_name = db.StringField(required=True)
     role = db.StringField(choices=USER_ROLE, required=True, immutable=True)
     parent = db.StringField(required=False, immutable=True)
     state = db.StringField(required=False)
     street = db.StringField(required=False)
     city = db.StringField(required=False)
     zip = db.StringField(required=False)
-
